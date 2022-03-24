d735ab02832bc910def0e05a7c627d52d24c813b
fra <fra.sette.p.c@gmail.com>
2022-03-24 09:54:12 +0100

    changes

--- Lab00/ex5.s
+++ Lab00/ex5.s
@@ -1,14 +1,15 @@
 # write 0 in $t0
 
 # in c
-;int a;
-;a = 0;
+# int a;
+# a = 0;
 
 .text
 .globl main
 .ent main
 
-main: and $t0, $0, $0
+main: and $t1, $0, $0
+
       li $v0, 10
       syscall
 .end main
--- Lab00/ex9.s
+++ Lab00/ex9.s
@@ -1,8 +1,7 @@
 # legge DIM elementi di un vettore e li visualizza sulla console (schermo) in ordine inverso
 
 # in c
-# char
-# vett[5];
+# char vett[5];
 # int i;
 #
 # for(i=0; i<5; i++)
@@ -21,6 +20,7 @@ vett: .space DIM
 
 main: la $t2, vett
       and $t3, $0, $0     # index
+
 lab1: li $v0, 12          # read one character
       syscall
 
@@ -30,7 +30,7 @@ lab1: li $v0, 12          # read one character
       bne $t3, DIM, lab1  # repeat 5 times
 
 lab2: addi $t3, $t3, -1
-      addi $t2, $t2, -1lb
+      addi $t2, $t2, -1
 
       lb $a0, ($t2)
       li $v0, 11          # write one character
@@ -38,8 +38,8 @@ lab2: addi $t3, $t3, -1
 
       bne $t3, 0, lab2    # repeat 5 times
 
-      li
-      syscall$v0, 10
+      li $v0, 10
+      syscall
 
 .end main
 
